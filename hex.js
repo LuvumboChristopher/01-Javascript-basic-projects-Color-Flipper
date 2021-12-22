14dc1d75a3cfb5b7b948cb3eed90e90bbc764aa5
Christopher Luvumbo <christopherluvumbo@gmail.com>
2021-12-22 15:43:01 +0100

    delete not used code

--- hex.js
+++ hex.js
@@ -1,8 +1,9 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+/* #123456 */
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-const current = document.querySelector(".current");
 
 const getRandomNumber = () => Math.floor(Math.random() * hex.length);
 
@@ -14,8 +15,3 @@ btn.addEventListener("click",  ()=> {
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
-
-
-current.addEventListener('click', ()=> {
-    
-})
