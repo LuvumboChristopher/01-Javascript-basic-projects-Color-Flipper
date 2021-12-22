eb2769d143c9fd82f91df71d27e4f6a1e59e4c17
Christopher Luvumbo <christopherluvumbo@gmail.com>
2021-12-22 15:06:41 +0100

    first commit

---
+++ hex.js
@@ -0,0 +1,21 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const current = document.querySelector(".current");
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
+
+btn.addEventListener("click",  ()=> {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+
+current.addEventListener('click', ()=> {
+    
+})
